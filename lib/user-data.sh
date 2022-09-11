f7e5155389ca9faaae9bca71618d14021c23d314
big-vi <vignesh.murugan@hotmail.com>
2022-09-11 16:50:20 +1200

    user data for ec2 instance to install initial setup k8s

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -1,13 +1,63 @@
 #!/bin/bash
 sudo su
-yum update -y
+apt-get update
 
-amazon-linux-extras install -y nginx1
-systemctl start nginx
-systemctl enable nginx
+# hostnamectl set-hostname k8smaster
 
-chmod 2775 /usr/share/nginx/html
-find /usr/share/nginx/html -type d -exec chmod 2775 {} \;
-find /usr/share/nginx/html -type f -exec chmod 0664 {} \;
+# Disable SELinux
+# setenforce 0
+# sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
-echo "<h1>It worked</h1>" > /usr/share/nginx/html/index.html
+# Installing Docker
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+service docker start
+
+# Installing kubeadm, kubectl, kubelet
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+# Enable the use of iptables 
+echo "net.bridge.bridge-nf-call-iptables=1" | tee -a /etc/sysctl.conf
+sysctl -p
+
+#Install cri-dockerd
+###Install GO###
+wget https://storage.googleapis.com/golang/getgo/installer_linux
+chmod +x ./installer_linux
+./installer_linux
+source ~/.bash_profile
+
+cd cri-dockerd
+mkdir bin
+go build -o bin/cri-dockerd
+mkdir -p /usr/local/bin
+install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+cp -a packaging/systemd/* /etc/systemd/system
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
+
+rm /etc/containerd/config.toml
+systemctl restart containerd
